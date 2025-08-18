378a0f648adf50c4ff2618f04a8580305ed93ef7
CN-tsy <tsyincq@outlook.com>
2025-08-18 10:58:07 +0800

    Update JavaS.js

--- JavaS.js
+++ JavaS.js
@@ -1,1 +1,16 @@
-alert("由于作者正在学习JavaScript,手机端左上角的“三”需要JS检测,所以无法连接到导航栏,手机可以在设置里把ui设置成pc即可")
+console.log("你学会了打开开发者工具!");
+setTimeout(() => {
+    alert("由于作者正在学习JavaScript,手机端左上角的“三”需要JS检测,所以无法连接到导航栏,手机可以在设置里把ui设置成pc即可");
+    console.log('代码将在3秒后执行');
+}, 3000); // 3000毫秒=3秒
+document.addEventListener('DOMContentLoaded', function() {
+    const closeBtn = document.querySelector(".tip_close");
+    const tip = document.querySelector(".tips");
+    
+    function hide(e) {
+        e.preventDefault();
+        tip.style.display = 'none';
+    }
+    
+    closeBtn.addEventListener("click", hide);
+});
